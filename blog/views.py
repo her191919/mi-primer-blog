8500c552e9e9ea80ba3f92dd0ba93086396fdfb5
hernan <hernanberenguer19@gmail.com>
2025-06-09 16:51:06 -0300

    Mi aplicación proyecto Django, primer commit

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Publicacion
 from django.contrib.auth.models import User
+from django.urls import path
+from . import views
 # Create your views here.
 def lista_public(request):
 	publicaciones=Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('-fecha_publicacion')
