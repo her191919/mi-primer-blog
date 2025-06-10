cc76f1a0e91d992dc8dfe73017ee5c40a087946d
her191919 <hernanberenguer19@gmail.com>
2025-06-10 20:13:41 -0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 
-def main():
+def master():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ms.settings')
     try:
@@ -19,4 +19,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    master()
